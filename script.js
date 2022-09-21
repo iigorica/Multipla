40171defa5e1b659231564cb657d2a00960e8465
iigorica <pantelicigor294@gmail.com>
2022-09-21 20:05:53 +0200

    'final'

--- script.js
+++ script.js
@@ -7,3 +7,39 @@ menu.addEventListener('click', function(){
 });
 
 
+// Contact form
+
+    let errors = document.getElementById('error');
+
+
+function validateName() {
+    var name = document.getElementById('ime').value;
+
+    if(name.length == 0){
+        error.innerHTML = 'Ime je obavezno'
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        error.innerHTML = 'Napišite puno ime i prezime'
+        return false;
+    }
+    error.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true;
+    
+}
+
+function validateEmail() {
+    var email = document.getElementById('email').value;
+
+    if(email.length == 0){
+        error.innerHTML = 'Email je obavezan'
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        error.innerHTML = 'Napišite ispravan email';
+        return false;
+    }
+    error.innerHTML = '<i class="fas fa-check-circle"></i>'
+    return true;
+    
+}
